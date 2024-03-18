83a98a5a650c9e6d1fa7ca76d854b1104fe48bd6
Afroz Alam <afrozcpr@gmail.com>
2024-03-19 00:34:38 +0530

    string

---
+++ String_Builder.java
@@ -0,0 +1,32 @@
+//package String_Questions;
+//
+//public class String_Builder {
+//    public static void main(String[] args) {
+//        // Creating an empty StringBuilder
+//        StringBuilder SB = new StringBuilder();
+//
+//        // Appending strings to StringBuilder
+//        SB.append("Hello");
+//        SB.append(">>>>");
+//        SB.append("World");
+//
+//        // Printing the content of StringBuilder
+//        System.out.println("StringBuilder content: " + SB.toString());
+//
+////        // Inserting characters at specific positions
+////        stringBuilder.insert(5, ", ");
+////        System.out.println("After inserting comma: " + stringBuilder.toString());
+//
+//        // Deleting characters from StringBuilder
+//        SB.delete(5, 7); // Deletes characters from index 5 to index 7
+//        System.out.println("After deleting comma: " + SB.toString());
+//
+//        // Reversing the content of StringBuilder
+////        stringBuilder.reverse();
+////        System.out.println("After reversing: " + stringBuilder.toString());
+////
+////        // Setting a new value for StringBuilder
+////        stringBuilder.setLength(5);
+////        System.out.println("After setting length to 5: " + stringBuilder.toString());
+//    }
+//}
